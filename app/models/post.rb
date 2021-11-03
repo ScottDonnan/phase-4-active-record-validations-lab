81ce272d1428278ec9a3832e75a118b44864e776
Scott Donnan <scott.donnan@me.com>
2021-11-03 09:05:09 -0500

    commit tree bdfb40586a959875bf53e595b45e255c3a630358

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    # validate :sufficiently_clickbaity
+
+    # def sufficiently_clickbaity
+    #     if title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top") || title.include?("Guess")
+    #         errors.add(:title, "can't be in the past")
+    #     end
+    # end
 end
+
+
